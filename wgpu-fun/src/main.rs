a553f75f20cd0b8c1e047f26e034395550aa06d5
smpl <sreddy@hey.com>
2026-04-04 00:54:52 -0400

    .

--- wgpu-fun/src/main.rs
+++ wgpu-fun/src/main.rs
@@ -1,3 +1,105 @@
+// https://youtube.com/playlist?list=PLn3eTxaOtL2PNbW4ou-APMV9W9m6nppYl
+
+use glfw::{Action, Context, Key};
+
+struct State<'a> {
+    instance: wgpu::Instance,
+    surface: wgpu::Surface<'a>,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    size: (i32, i32),
+    window: &'a mut glfw::Window,
+}
+
+// contsructor
+impl<'a> State<'a> {
+    async fn new(window: &'a mut glfw::Window) -> Self {
+        let size = window.get_framebuffer_size();
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor::new_without_display_handle());
+        let target = unsafe { wgpu::SurfaceTargetUnsafe::from_window(&window) }.unwrap();
+        let surface = unsafe { instance.create_surface_unsafe(target) }.unwrap();
+
+        let adapter_descriptor = wgpu::RequestAdapterOptionsBase {
+            power_preference: wgpu::PowerPreference::default(),
+            compatible_surface: Some(&surface),
+            force_fallback_adapter: false,
+        };
+
+        let adapter = instance.request_adapter(&adapter_descriptor).await.unwrap();
+
+        let device_descriptor = wgpu::DeviceDescriptor {
+            label: Some("Device"),
+            required_features: wgpu::Features::default(),
+            required_limits: wgpu::Limits::default(),
+            experimental_features: wgpu::ExperimentalFeatures::default(),
+            memory_hints: wgpu::MemoryHints::default(),
+            trace: wgpu::Trace::default(),
+        };
+        let (device, queue) = adapter.request_device(&device_descriptor).await.unwrap();
+
+        let surface_capabilities = surface.get_capabilities(&adapter);
+        let surface_format = surface_capabilities
+            .formats
+            .iter()
+            .copied()
+            .filter(|texture_format| texture_format.is_srgb())
+            .next()
+            .unwrap_or(surface_capabilities.formats[0]);
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.0 as u32,
+            height: size.1 as u32,
+            present_mode: surface_capabilities.present_modes[0],
+            alpha_mode: surface_capabilities.alpha_modes[0],
+            view_formats: vec![],
+            desired_maximum_frame_latency: 2,
+        };
+
+        Self {
+            instance,
+            size,
+            surface,
+            device,
+            queue,
+            window,
+            config,
+        }
+    }
+}
+
+async fn run() {
+    use glfw::fail_on_errors;
+    let mut glfw = glfw::init(fail_on_errors!()).unwrap();
+
+    let (mut window, events) = glfw
+        .create_window(300, 300, "Hello GLFW", glfw::WindowMode::Windowed)
+        .unwrap();
+
+    // make the window's context current
+    window.make_current();
+    window.set_key_polling(true);
+
+    while !window.should_close() {
+        window.swap_buffers(); // wtf is this?
+        glfw.poll_events();
+        for (_, event) in glfw::flush_messages(&events) {
+            match event {
+                glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+                    window.set_should_close(true);
+                }
+                glfw::WindowEvent::Key(Key::Q, _, Action::Press, _) => {
+                    window.set_should_close(true);
+                }
+                _ => {}
+            }
+        }
+    }
+
+    println!("WGPU FUN");
+}
+
 fn main() {
-    println!("Hello, world!");
+    run();
 }
