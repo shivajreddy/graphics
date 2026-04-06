f18bf7f9ad0aba5ae8281de4b7b8b15b5cb2ae77
smpl <sreddy@hey.com>
2026-04-06 03:32:43 -0400

    .

--- learn-wgpu/learn-wgpu-2/src/main.rs
+++ learn-wgpu/learn-wgpu-2/src/main.rs
@@ -1,3 +1,5 @@
+#![allow(unused)]
+
 use std::sync::Arc;
 
 use winit::{
@@ -13,76 +15,34 @@ struct State {
     window: Arc<Window>,
 }
 
-impl State {
-    async fn new(window: Arc<Window>) -> anyhow::Result<Self> {
-        Ok(Self { window })
-    }
-
-    fn resize(&mut self, size: PhysicalSize<u32>) {}
-}
-
 struct App {
     state: Option<State>,
 }
 
 impl App {
-    fn new(event_loop: &EventLoop<State>) -> Self {
+    fn new() -> Self {
         Self { state: None }
     }
 }
 
 impl ApplicationHandler<State> for App {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        let window_attributes = Window::default_attributes();
-        let window = Arc::new(event_loop.create_window(window_attributes).unwrap());
-        self.state = Some(pollster::block_on(State::new(window)).unwrap());
-    }
-
-    fn user_event(&mut self, _event_loop: &ActiveEventLoop, mut event: State) {
-        self.state = Some(event);
+        todo!()
     }
 
     fn window_event(
         &mut self,
         event_loop: &ActiveEventLoop,
         window_id: window::WindowId,
-        event: winit::event::WindowEvent,
+        event: WindowEvent,
     ) {
-        let state = match &mut self.state {
-            Some(canvas) => canvas,
-            None => return,
-        };
-        match event {
-            WindowEvent::CloseRequested => event_loop.exit(),
-            WindowEvent::Resized(size) => state.resize(size),
-            WindowEvent::RedrawRequested => {}
-            WindowEvent::KeyboardInput {
-                event:
-                    KeyEvent {
-                        physical_key: PhysicalKey::Code(code),
-                        state: key_state,
-                        ..
-                    },
-                ..
-            } => match (code, key_state.is_pressed()) {
-                (KeyCode::Escape, true) => event_loop.exit(),
-                (KeyCode::KeyQ, true) => event_loop.exit(),
-                _ => {}
-            },
-            _ => {}
-        }
+        todo!()
     }
 }
 
-fn run() -> anyhow::Result<()> {
-    env_logger::init();
-    let event_loop: EventLoop<State> = EventLoop::with_user_event().build()?;
-    let mut app = App::new(&event_loop);
-    event_loop.run_app(&mut app)?;
-    Ok(())
-}
+fn run_window() {}
 
 fn main() {
-    println!("Learn Wpgu - 2");
-    run().unwrap();
+    println!("redoing surface");
+    run_window();
 }
