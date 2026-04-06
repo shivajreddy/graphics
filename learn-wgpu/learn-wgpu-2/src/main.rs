c2888a56bfaecdf3032c8eb3a32e356c591e2ef5
smpl <sreddy@hey.com>
2026-04-06 12:49:09 -0400

    .

--- learn-wgpu/learn-wgpu-2/src/main.rs
+++ learn-wgpu/learn-wgpu-2/src/main.rs
@@ -5,7 +5,7 @@ use std::sync::Arc;
 use winit::{
     application::ApplicationHandler,
     dpi::PhysicalSize,
-    event::{KeyEvent, WindowEvent},
+    event::{Event, KeyEvent, WindowEvent},
     event_loop::{self, ActiveEventLoop, EventLoop},
     keyboard::{KeyCode, PhysicalKey},
     window::{self, Window},
@@ -14,33 +14,73 @@ use winit::{
 struct State {
     window: Arc<Window>,
 }
+impl State {
+    async fn new(window: Arc<Window>) -> anyhow::Result<Self> {
+        Ok(Self { window })
+    }
+}
 
 struct App {
     state: Option<State>,
 }
 
 impl App {
-    fn new() -> Self {
+    fn new(event_loop: &EventLoop<State>) -> Self {
         Self { state: None }
     }
 }
 
 impl ApplicationHandler<State> for App {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        todo!()
+        let window_attribures = Window::default_attributes();
+        let window = event_loop.create_window(window_attribures).unwrap();
+        let state = pollster::block_on(State::new(Arc::new(window))).unwrap();
+        self.state = Some(state);
     }
 
     fn window_event(
         &mut self,
         event_loop: &ActiveEventLoop,
         window_id: window::WindowId,
-        event: WindowEvent,
+        event: winit::event::WindowEvent,
     ) {
-        todo!()
+        let state = match &mut self.state {
+            Some(canvas) => canvas,
+            None => return,
+        };
+        match event {
+            WindowEvent::CloseRequested => event_loop.exit(),
+            // WindowEvent::Resized(size) => state.resize(size),
+            WindowEvent::RedrawRequested => {}
+            WindowEvent::KeyboardInput {
+                event:
+                    KeyEvent {
+                        physical_key: PhysicalKey::Code(code),
+                        state: key_state,
+                        ..
+                    },
+                ..
+            } => match (code, key_state.is_pressed()) {
+                (KeyCode::Escape, true) => event_loop.exit(),
+                (KeyCode::KeyQ, true) => event_loop.exit(),
+                _ => {}
+            },
+            _ => {}
+        }
+    }
+
+    fn user_event(&mut self, _event_loop: &ActiveEventLoop, mut event: State) {
+        self.state = Some(event);
     }
 }
 
-fn run_window() {}
+fn run_window() -> anyhow::Result<()> {
+    env_logger::init();
+    let event_loop: EventLoop<State> = EventLoop::with_user_event().build()?;
+    let mut app = App::new(&event_loop);
+    event_loop.run_app(&mut app);
+    Ok(())
+}
 
 fn main() {
     println!("redoing surface");
