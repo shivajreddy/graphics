bfe3f72afaa7289dfc6ceaf702bdc7c2866c65ee
smpl <sreddy@hey.com>
2026-04-06 23:35:45 -0400

    .

--- learn-wgpu/learn-wgpu-2/src/main.rs
+++ learn-wgpu/learn-wgpu-2/src/main.rs
@@ -12,11 +12,81 @@ use winit::{
 };
 
 struct State {
+    // window related
     window: Arc<Window>,
+
+    // surface related
+    surface: wgpu::Surface<'static>,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    is_surface_configured: bool,
 }
+
 impl State {
     async fn new(window: Arc<Window>) -> anyhow::Result<Self> {
-        Ok(Self { window })
+        let size = window.inner_size();
+        // - instance: purpose is to create 'adapter' & 'surface'
+        let instance = wgpu::Instance::new(wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::default(),
+            flags: wgpu::InstanceFlags::default(),
+            memory_budget_thresholds: wgpu::MemoryBudgetThresholds::default(),
+            backend_options: wgpu::BackendOptions::default(),
+            display: None,
+        });
+        // - adapter: its a handle to the actual GPU, used to create 'Device' & 'Queue'
+        // - surface: its where the window can draw to
+        let surface = instance.create_surface(window.clone()).unwrap();
+        // let adapter = instance
+        //     .request_adapter(&wgpu::RequestAdapterOptions {
+        //         power_preference: wgpu::PowerPreference::default(),
+        //         compatible_surface: Some(&surface),
+        //         force_fallback_adapter: false,
+        //     })
+        //     .await?;
+        let adapter = instance
+            .enumerate_adapters(wgpu::Backends::all())
+            .await
+            .into_iter()
+            .filter(|adapter| adapter.is_surface_supported(&surface))
+            .next()
+            .unwrap();
+        let (device, queue) = adapter
+            .request_device(&wgpu::DeviceDescriptor {
+                label: None,
+                required_features: wgpu::Features::empty(),
+                required_limits: wgpu::Limits::default(),
+                experimental_features: wgpu::ExperimentalFeatures::disabled(),
+                memory_hints: Default::default(),
+                trace: wgpu::Trace::Off,
+            })
+            .await?;
+        let surface_caps = surface.get_capabilities(&adapter);
+        let surface_format = surface_caps
+            .formats
+            .iter()
+            .find(|surf| surf.is_srgb())
+            .copied()
+            .unwrap_or(surface_caps.formats[0]);
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.width,
+            height: size.height,
+            present_mode: surface_caps.present_modes[0],
+            alpha_mode: surface_caps.alpha_modes[0],
+            desired_maximum_frame_latency: 2,
+            view_formats: vec![],
+        };
+
+        Ok(Self {
+            window,
+            surface,
+            device,
+            queue,
+            config,
+            is_surface_configured: false,
+        })
     }
 }
 
